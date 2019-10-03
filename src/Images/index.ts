4f3f667b3e815866bafb17d3b9ae0382658f97a4
Nich Secord <nicholassecord@gmail.com>
2019-10-03 10:07:45 -0700

    routed images through index file

---
+++ src/Images/index.ts
@@ -0,0 +1,70 @@
+import ATL from './atl.gif';
+import AZ from './az.gif';
+import BAL from './bal.gif';
+import BUF from './buf.gif';
+import CAR from './car.gif';
+import CHI from './chi.gif';
+import CIN from './cin.gif';
+import CLE from './cle.gif';
+import DAL from './dal.gif';
+import DEN from './den.gif';
+import DET from './det.gif';
+import GB from './gb.gif';
+import HOU from './hou.gif';
+import IND from './ind.gif';
+import JAC from './jac.gif';
+import KC from './kc.gif';
+import LAC from './lac.gif';
+import LAR from './lar.gif';
+import MIA from './mia.gif';
+import MIN from './min.gif';
+import NE from './ne.gif';
+import NO from './no.gif';
+import NYG from './nyg.gif';
+import NYJ from './nyj.gif';
+import OAK from './oak.gif';
+import PHI from './phi.gif';
+import PIT from './pit.gif';
+import SEA from './sea.gif';
+import SF from './sf.gif';
+import TB from './tb.gif';
+import TEN from './ten.gif';
+import WAS from './was.gif';
+
+import NFL from './nfl.gif';
+
+export {
+  ATL,
+  AZ,
+  BAL,
+  BUF,
+  CAR,
+  CHI,
+  CIN,
+  CLE,
+  DAL,
+  DEN,
+  DET,
+  GB,
+  HOU,
+  IND,
+  JAC,
+  KC,
+  LAC,
+  LAR,
+  MIA,
+  MIN,
+  NE,
+  NO,
+  NYG,
+  NYJ,
+  OAK,
+  PHI,
+  PIT,
+  SEA,
+  SF,
+  TB,
+  TEN,
+  WAS,
+  NFL,
+};
